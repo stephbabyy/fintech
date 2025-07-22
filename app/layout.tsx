8c883477de5155e417c14982ecfd385c03c4f1f5
stephbabyy <149121442+stephbabyy@users.noreply.github.com>
2025-07-22 17:16:07 +0100

    🚧 WIP: added general sans font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { MaxWidthWrapper, ThemeProvider } from "@/lib";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <MaxWidthWrapper>
+              {children}
+            </MaxWidthWrapper>
+        </ThemeProvider>
       </body>
     </html>
   );
